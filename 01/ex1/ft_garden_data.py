e8c03ef1a8d7387c6419dd4db4a48e1961a80a7b
emi <emilienwaltz@gmail.com>
2026-02-28 15:29:49 +0100

    finish 00

--- 01/ex1/ft_garden_data.py
+++ 01/ex1/ft_garden_data.py
@@ -5,24 +5,16 @@ class Plant:
 		self.height = height
 		self.age = age
 
-class Garden:
-	def __init__(self, name:str, size: int) -> None:
-		self.plants: list[Plant] = []
-		self.name = name
-		self.size = size
-	def add(self, plant: Plant) -> None:
-        self.plants.append(plant)
-
-    def display(self) -> None:
-        for i in range(len(self.plants)):
-            p = self.plants[i]
-            print(p.name)
-
 def	main():
 	p1 = Plant("tulipe", 21, 60)
 	p2 = Plant("tournesol", 34, 12)
 	p3 = Plant("coquelicot", 5, 34)
-	print(p1.name)
+	plants = [p1, p2 , p3]
+	for plant in plants:
+		plant.name = plant.name.capitalize()
+	print("=== Garden Plant Registry ===")
+	for plant in plants:
+		print(f"{plant.name}: {plant.height}cm, {plant.age} days old")
 
 if __name__ == "__main__":
 	main()
