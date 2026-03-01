83e1068de675c00f2571be50611546cbfc7b8fa6
emi <emilienwaltz@gmail.com>
2026-03-01 14:12:05 +0100

    jsp

--- 01/ex4/ft_garden_security.py
+++ 01/ex4/ft_garden_security.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python3
-class Plant:
+
+class SecurePlant:
     def __init__(self, name: str, height: int, age_days: int) -> None:
         self.name = name
-        self.height = height
-        self.age_days = age_days
+        self._height = 0
+        self._age_days = 0
+        self.set_height(height)
+        self.set_age(age_days)
 
-    if name < 0:
-        return("error")
-    def grow(self):
-        self.height = self.height + 1
+    def set_height(self, height: int) -> None:
+        if height < 0:
+            print("[SECURITY] Rejected height:", height)
+            return
+        self._height = height
+        print("Height updated:", self._height)
 
-    def age(self):
-        self.age_days = self.age_days + 1
+    def set_age(self, age_days: int) -> None:
+        if age_days < 0:
+            print("[SECURITY] Rejected age:", age_days)
+            return
+        self._age_days = age_days
+        print("Age updated:", self._age_days)
 
-    def get_info(self):
-        return (f"{self.name}: ({self.height}cm, {self.age_days} days)")
+    def get_height(self) -> int:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age_days
+
+    def get_info(self) -> str:
+        return f"{self.name}: ({self._height}cm, {self._age_days} days)"
 
 
 def main():
-    names = ["Rose", "Tulipe", "Tournesol", "Cactus", "Coquelicot"]
-    heights = [12, 13, 50, 120, 20]
-    age_days = [4, 12, 43, 23, 40]
-    garden_size = 5
-    plants = [None] * garden_size
-    for i in range(garden_size):
-        plant = Plant(names[i], heights[i], age_days[i])
-        plants[i] = plant
+    print("=== Garden Security System ===")
 
-    print("=== Plant Factory Output ===")
-    for i in range(garden_size):
-        disp_info = plants[i].get_info()
-        print(f"Created: {disp_info}")
-    print(f"Total plant created: {garden_size}")
+    plant = SecurePlant("Coquelicot", 2, 3)
+    print("Current:", plant.get_info())
+
+    plant.set_height(-2)
+    plant.set_age(-1)
+
+    plant.set_height(12)
+
+    print("Final:", plant.get_info())
 
 
 if __name__ == "__main__":
