997882d5183823415ee5263f538d3c637d4d3fbe
emi <emilienwaltz@gmail.com>
2026-03-06 18:01:25 +0100

    almost finish 01

--- 01/ex6 /ft_garden_analytics.py
+++ 01/ex6 /ft_garden_analytics.py
@@ -1,25 +1,26 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_garden_analytics.py                             :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: ewaltz <ewaltz@student.42.fr>              +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/03/06 13:06:18 by ewaltz            #+#    #+#              #
-#    Updated: 2026/03/06 13:08:11 by ewaltz           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/usr/bin/env python3
 
-class GardenManager:
-    def __init__(self, action_to_do: str):
 
-        def GardenStat
+class GardenManager:
+    def __init__(self, gardens: list):
+        self.gardens = gardens
 
 
 class Garden:
-    def __init__(self, name: str, plants: list)
+    def __init__(self, name: str, plants: list) -> None:
+        self.name = name
+        self.plants = plants
+
+    def add_plant(self, plant: list) -> None:
+        self.plants += [plant]
+
+    def grow_all(self):
+        for plant in self.plants:
+            plant.grow()
+
+    def get_info(self):
+        for plant in self.plants:
+            plant.get_info()
 
 
 class Plant:
@@ -27,31 +28,73 @@ class Plant:
         self.name = name
         self.height = height
         self.days = days
-        self.type = "Undefined"
+        self.type = "Standard"
 
     def grow(self):
         self.height += 1
         self.days += 1
+
+    def get_info(self):
+        print(f"{self.name} Tree: {self.height}cm")
 
 
 class FloweringPlant(Plant):
     def __init__(self, name: str, height: int, days: int, color: str) -> None:
         super().__init__(name, height, days)
         self.color = color
-
+        self.type = "Flower"
+        self.blooming = "Not blooming yet"
         if self.days > 7:
-            self.blooming = True
-        else:
-            self.blooming = False
+            self.blooming = "Blooming"
+
+    def get_info(self):
+        print(f"{self.name}: {self.height}cm, {self.color} flower, ({self.blooming})")
 
 
 class PrizeFlower(FloweringPlant):
     def __init__(self, name: str, height: int, days: int, color: str) -> None:
         super().__init__(name, height, days, color)
         self.prize = self.height * self.days
+        self.type = "Prize Flower"
+
+    def get_info(self):
+        print(
+            f"{self.name}: {self.height}cm, {self.color} flower, ({self.blooming}), Prize points: {self.prize}"
+        )
+
+
+# def main():
+#     p1 = Plant("tulipe", 20, 10)
+#     p2 = Plant("coquelicot", 20, 10)
+#     p3 = Plant("tournesol", 20, 10)
+#     plants = [p1, p2, p3]
+#     garden1 = Garden("emi", plants)
+#     print(f"{garden1.name}")
+#     garden1.get_info()
 
 
 def main():
+    p1 = Plant("Oak", 20, 10)
+    p2 = FloweringPlant("Rose", 25, 8, "red")
+    p3 = PrizeFlower("Sunflower", 50, 12, "yellow")
+
+    plants = [p1, p2, p3]
+    garden1 = Garden("Emi", plants)
+
+    print("=== Garden name ===")
+    print(garden1.name)
+
+    print("\n=== Initial state ===")
+    garden1.get_info()
+
+    print("\n=== After grow_all ===")
+    garden1.grow_all()
+    garden1.get_info()
+
+    print("\n=== Add one plant ===")
+    p4 = Plant("Cactus", 15, 4)
+    garden1.add_plant(p4)
+    garden1.get_info()
 
 
 if __name__ == "__main__":
